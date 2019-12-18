28d50cfc56f8adbc12e6df45d1ed88ac0941bd02
nesuma <hjd-97@web.de>
2019-12-18 22:52:27 +0100

    two best results, one for further calculation, one as final result. Also refactoring

--- src/mlulsp/solvers/ga/GaSolver.java
+++ src/mlulsp/solvers/ga/GaSolver.java
@@ -22,7 +22,7 @@ public class GaSolver implements Solver {
     // instance ist eine Instanz des Entscheidungsproblems
     public ProductionSchedule solve(Instance instance) {
         int anz = 10;
-        Individual best;
+        Individual finalBest,tempBest;
 
         Population population = new Population(anz, instance);
         population.firstLastPeriodsBerechnen();
@@ -32,41 +32,43 @@ public class GaSolver implements Solver {
         population.initialize();
 
         // Standard Vergleichswert erschaffen
-        best = new Individual(instance);
-        best.initRandom();
-        best.decoding(instance);
-        best.evaluate();
+        finalBest = new Individual(instance);
+		finalBest.initRandom();
+		finalBest.decoding(instance);
+		finalBest.evaluate();
+        tempBest = new Individual(instance);
+        tempBest.initRandom();
+		tempBest.decoding(instance);
+		tempBest.evaluate();
+
         System.out.println("Aufgabe " + instance.getName());
-        System.out.println("Startwert = " + best.getFitness());
+        System.out.println("Startwert = " + finalBest.getFitness());
 
-        population.setBestIndividual(best);
+        population.setBestIndividual(finalBest);
 
-        // fortpflanzen alle mit alle? Oder selektion der besten wahrscheinlicher oder ganz random
-
-        // v1 one pop of 20 for 10 children + 10 parents, children replace parents
-        // v2 one pop of 20 for 10 children + 10 parents, new 10 replace worst? Best? middle?
-
-        // for Schleife für alle Lösungen
-
-        int iterations = anzahlLoesungen / anz;
-        int step_one = (int)(iterations * 0.1);
-        int step_two = (int)(iterations * 0.70);
-        int step_three = (int)(iterations * 0.75);
+		int iterations = anzahlLoesungen / anz;
+		int step_one = (int)(iterations * 0.05);
+		int step_two = (int)(iterations * 0.1);
+		int step_three = (int)(iterations * 0.7);
+		int step_four = (int)(iterations * 0.75);
 
         double pMut = population.getMutationProbability();
 
-        population.setMutationProbability(pMut);
+		Individual.setMut(20*pMut);
 
-        for (int i = 1; i < iterations; i++) {
-            if(iterations == step_one){
-                population.setMutationProbability(pMut);
-            }
-            if(iterations ==step_two){
-                population.setMutationProbability(30*pMut);
-            }
-            if(iterations == step_three){
-                population.setMutationProbability(pMut);
-            }
+		for (int i = 1; i < iterations; i++) {
+			if(iterations == step_one){
+				Individual.setMut(pMut);
+			}
+			if(iterations ==step_two){
+				Individual.setMut(pMut);
+			}
+			if(iterations == step_three){
+				Individual.setMut(20*pMut);
+			}
+			if(iterations == step_four){
+				Individual.setMut(pMut);
+			}
 
             Individual[] children = new Individual[anz];
             population.rankRouletteSelection();
@@ -74,24 +76,27 @@ public class GaSolver implements Solver {
             for (int j = 0; j < population.getSize(); j++) {
                 Individual parent_a = population.getEntity();
                 Individual parent_b = population.getEntity();
-                children[j] = parent_a.mate(parent_b);
+                children[j] = parent_a.mate(parent_b,instance);
                 children[j].mutate(population.getMutationProbability());
                 children[j].decoding(instance);
                 children[j].evaluate();
             }
 
-            population.setNewPopulation(children);
-            for (Individual child : children){
-                if (child.getFitness() < best.getFitness()) {
-//                    System.out.println("Neuer bester Wert = " + (1/best.getFitness()));
-                    best = child;
-                }
-            }
-            population.setBestIndividual(best);
-        }
+			population.setNewPopulation(children);
+			for (Individual child : children){
+				if (child.getFitness() < tempBest.getFitness()) {
+                    System.out.println("Neuer bester Wert = " + (child.getFitness()));
+					tempBest = child;
+					finalBest = Individual.getCloneOfIndividual(child,instance);
+				}
+			}
+			population.setBestIndividual(tempBest);
+		}
+		finalBest.decoding(instance);
+		finalBest.evaluate();
+		System.out.println("best fitness:" + finalBest.getFitness());
+		finalBest.ausgabe(instance);
+		return finalBest.getPhaenotype();
 
-        best.ausgabe(instance);
-        return best.getPhaenotype();
-
-    }
+	}
 }
